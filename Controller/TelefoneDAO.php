8f1447aee48461e54d753d854c1bbda72de539e3
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2018-01-24 16:32:23 -0200

    Update TelefoneDAO.php

--- Controller/TelefoneDAO.php
+++ Controller/TelefoneDAO.php
@@ -18,5 +18,19 @@
       $result = mysqli_query($this->con, $sql);
       return $result;
     }
+    
+     public function update($fields,$params=null,$where=null){
+        $sql = "UPDATE telefone SET ( ".$fields." = ".$params" )";
+        if(isset($where)) $sql ." WHERE ".$where;
+        $result = mysqli_query($this->con, $sql);
+        return $result;
+    }
+    
+    public function delete($where=null,$params=null){
+        $sql = "DELETE FROM telefone";
+        if(isset($where)) $sql ." WHERE " .$where. " = ".$params;
+         $result = mysqli_query($this->con, $sql);
+        return $result;
+    }
   }
 ?>
