2f432a82c0c1c07ca0da344df26c1bb7a74d1bd7
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2018-01-24 16:31:47 -0200

    Update EnderecoDAO.php

--- Controller/EnderecoDAO.php
+++ Controller/EnderecoDAO.php
@@ -18,5 +18,19 @@
       $result = mysqli_query($this->con, $sql);
       return $result;
     }
+    
+     public function update($fields,$params=null,$where=null){
+        $sql = "UPDATE endereco SET ( ".$fields." = ".$params" )";
+        if(isset($where)) $sql ." WHERE ".$where;
+        $result = mysqli_query($this->con, $sql);
+        return $result;
+    }
+    
+    public function delete($where=null,$params=null){
+        $sql = "DELETE FROM endereco";
+        if(isset($where)) $sql ." WHERE " .$where. " = ".$params;
+         $result = mysqli_query($this->con, $sql);
+        return $result;
+    }
   }
 ?>
