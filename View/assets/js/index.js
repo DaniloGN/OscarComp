93afba9816ba9113e84e33f91d4fa52f0d733999
KatoMono Enkeli <k4t0mono@gmail.com>
2018-01-31 17:07:43 -0200

    Teste modal

--- View/assets/js/index.js
+++ View/assets/js/index.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-  $("#estatueta_div img").click(function() {
-    // $("#cortina").fadeOut(2000);
-    $("#cortina").hide();
-    // $(".hiden").fadeIn(3500);
-    $(".hiden").show();
-  });
-
-  $("a").click(function() {
-    console.log("fff");
-    $("#cortina").animate({height:'0px'});
-    $("#texto_cortina").fadeOut(2000);
-    $(".hiden").fadeIn(2500);
+  // $("#estatueta_div img").click(function() {
+  //   // $("#cortina").fadeOut(2000);
+  //   $("#cortina").hide();
+  //   // $(".hiden").fadeIn(3500);
+  //   $(".hiden").show();
+  // });
+  //
+  // $("a").click(function() {
+  //   console.log("fff");
+  //   $("#cortina").animate({height:'0px'});
+  //   $("#texto_cortina").fadeOut(2000);
+  //   $(".hiden").fadeIn(2500);
+  // })
+  $('.modal').modal();
+  $("#login").click(function() {
+    console.log("ffgg");
+    $('#modal1').modal('open');
   })
 });
