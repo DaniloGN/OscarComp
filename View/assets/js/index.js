77f8c73f7a3b17761089dd069411555162569653
KatoMono Enkeli <k4t0mono@gmail.com>
2018-02-01 00:29:40 -0200

    Corrigindo modal

--- View/assets/js/index.js
+++ View/assets/js/index.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
-  // $("#estatueta_div img").click(function() {
-  //   // $("#cortina").fadeOut(2000);
-  //   $("#cortina").hide();
-  //   // $(".hiden").fadeIn(3500);
-  //   $(".hiden").show();
-  // });
-  //
-  // $("a").click(function() {
-  //   console.log("fff");
-  //   $("#cortina").animate({height:'0px'});
-  //   $("#texto_cortina").fadeOut(2000);
-  //   $(".hiden").fadeIn(2500);
-  // })
   $('.modal').modal();
-  $('#modal_login').modal('open');
 
   $("#login").click(function() {
     console.log("ffgg");
     $('#modal_login').modal('open');
-  })
+  });
 });
