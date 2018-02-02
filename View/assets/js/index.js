696ccf6ce413f45c1d9b7c9cf28ac16c3e753137
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2018-02-02 16:37:49 -0200

    ajax para login no index.js
    foi feita uma requisição  para login.

--- View/assets/js/index.js
+++ View/assets/js/index.js
@@ -2,29 +2,31 @@ $(document).ready(function() {
   $('.modal').modal();
 
   $("#login").click(function() {
-    console.log("ffgg");
+    $('#errolog').hide();
     $('#modal_login').modal('open');
   });
-});
 
 
-$("#modal_login_send").click(function()
-{
-  var campoEmail = $("#email").val();
-  var campoSenha = $("#senha").val();
+  $('#errolog').hide();
+  $("#modal_login_send").click(function()
+  {
+    var campoEmail = $("#email").val();
+    var campoSenha = $("#senha").val();
 
-  $.ajax({
-      url: "../Controller/LoginInscrito.php",
-      type: 'POST',
-      data: { 'email' : campoEmail, 'senha' : campoSenha},
-      success: function(data) {
-        if ( data == "ok")
-        {
-          window.location.href = "../View/index.html";
-        }else
-        {
+   $.ajax({
+        url: "../Controller/LoginInscrito.php",
+        type: 'post',
+        data: { 'email' : campoEmail, 'senha' : campoSenha},
+        success: function(result) {
+          window.location.href = "./indexLogado.html"; 
+          console.log(result);
+        },
+        error: function(error){
+          $('#errolog').show();
+        }
       }
-    }
-
+    );
+    return false;
   });
 });
+
