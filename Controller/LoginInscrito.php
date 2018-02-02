09e8908c66a3dd480058e1db3eebe02b100a87c4
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2018-02-02 16:42:25 -0200

    update loginInscrito.php
    foi criada uma função response para a requisição do ajax.

--- Controller/LoginInscrito.php
+++ Controller/LoginInscrito.php
@@ -1,27 +1,37 @@
 <?php
-	include("./config.php");
-	include("./LoginDAO.php");
-	include("../Model/Login.php");
+include("./config.php");
+include("./LoginDAO.php");
+include("./InscritoDAO.php");
+include("../Model/Login.php");
 
-	$login = new Login($_POST["email"],$_POST["senha"]);
-	
-	$con = mysqli_connect($host, $user, $senha, $bd);
 
-	$loginDAO = new LoginDAO($con);
-	$inscritoDAO = new InscritoDAO($con);
+function response($body,$code){
+	http_response_code($code);
+	echo json_encode($body,JSON_PRETTY_PRINT)."\n";
+	exit();
+}
 
-	$loadLogin = $loginDAO->load("idLOGIN, EMAIL","where EMAIL = '".$login->getEmail()."' and SENHA = '".$login->getSenha()."'");
-	$vetor = mysqli_fetch_array($loadLogin, MYSQLI_ASSOC);
-	$loadInscrito = $inscritoDAO->load("NOME","where LOGIN_idLOGIN = ".$vetor["idLOGIN"]);
+$login = new Login($_POST["email"],$_POST["senha"]);
+
+$con = mysqli_connect($host, $user, $senha, $bd);
+
+$loginDAO = new LoginDAO($con);
+$inscritoDAO = new InscritoDAO($con);
+
+$loadLogin = $loginDAO->load("idLOGIN, EMAIL","where EMAIL = '".$login->getEmail()."' and SENHA = '".$login->getSenha()."'");
+$vetor = mysqli_fetch_array($loadLogin, MYSQLI_ASSOC);
+
+if($vetor){
+	$loadInscrito = $inscritoDAO->load("P_NOME","where LOGIN_idLOGIN = ".$vetor["idLOGIN"]);
 	$vetorInscrito = mysqli_fetch_array($loadInscrito, MYSQLI_ASSOC);
-	if($vetor){
-		session_start();
-		$_SESSION["EmailInscrito"]=$vetor["EMAIL"];
-		$_SESSION["NomeInscrito"]=$vetorInscrito["NOME"];
-		echo "ok";
-	}
-	else{
-		echo "erro";
-	}
+	session_start();
+	$_SESSION["idLogin"]=$vetor["idLOGIN"];
+	$_SESSION["EmailInscrito"]=$vetor["EMAIL"];
+	$_SESSION["NomeInscrito"]=$vetorInscrito["P_NOME"];
+	response("ok",200);
+}
+else{
+	response("erro",400);
+}
 
 ?>
