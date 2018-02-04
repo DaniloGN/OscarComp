d3e3ed2b9ef353b78e68c5ca2052bb3f303581bf
karolinelemos15 <godoykaroline@gmail.com>
2018-02-04 16:43:49 -0200

    tudo finalizado

---
+++ View/assets/js/indLog.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  $('.modal').modal();
+
+  $("#cat1").click(function() {
+    $('#modal_cat1').modal('open');
+  });
+});
+
+$(document).ready(function() {
+  $('.modal').modal();
+
+  $("#cat2").click(function() {
+    $('#modal_cat2').modal('open');
+  });
+});
+
+$(document).ready(function() {
+  $('.modal').modal();
+
+  $("#cat3").click(function() {
+    $('#modal_cat3').modal('open');
+  });
+});
+
+$(document).ready(function() {
+  $('.modal').modal();
+
+  $("#cat4").click(function() {
+    $('#modal_cat4').modal('open');
+  });
+});
+
+$(document).ready(function() {
+  $('.modal').modal();
+
+  $("#cat5").click(function() {
+    $('#modal_cat5').modal('open');
+  });
+});
+
+$(document).ready(function() {
+  $('.modal').modal();
+
+  $("#cat6").click(function() {
+    $('#modal_cat6').modal('open');
+  });
+});
